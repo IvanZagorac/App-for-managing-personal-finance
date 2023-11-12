5f84656099834650282ebf3d58d68c675d8e10a4
ivan.zagorac <ivan.zagorac@everymatrix.com>
2023-11-12 17:23:11 +0100

    Change backend url

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    pool: 'http://localhost:8085/',
+    pool: 'https://app-for-managing-personal-finance.onrender.com/',
     // https://gold-shy-lobster.cyclic.app
     // http://localhost:8085/'
 }
